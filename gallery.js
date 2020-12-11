44233502c490c83c7eada0d4ee89e4c3434b4198
DRAGONPR0N <75814098+DRAGONPR0N@users.noreply.github.com>
2020-12-11 03:08:01 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.getJSON("archive.json", function(json) {
         $.each(json.files, function(i, item) {
-            $(".gallery-content").append("<div class='gallery-item gallery-item" + i + "'></div>");
+            $(".gallery-content").append("<div class='gallery-item gallery-item" + i + " " + json.files[i].tag + "'></div>");
             $("<img>").attr("src", json.files[i].source).appendTo(".gallery-item" + i);
         });
     });
