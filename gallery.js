39a755317a821b0fb0d8b01e28416ba1eca5fb2b
DRAGONPR0N <75814098+DRAGONPR0N@users.noreply.github.com>
2020-12-11 16:24:42 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-    $.getJSON("archive.json", function(json) {
+    $.getJSON("https://raw.githubusercontent.com/DRAGONPR0N/dragonpr0n.github.io/main/archive.json", function(json) {
         $.each(json.files, function(i, item) {
-            $(".gallery-content").append("<div class='gallery-item gallery-item" + i + " " + json.files[i].tag + "'></div>");
-            $("<img>").attr("src", json.files[i].source).appendTo(".gallery-item" + i);
+            if(json.files[i].mimeType == "image") {
+                $(".gallery-content").append("<div class='gallery-item gallery-item" + i + "'></div>");
+                $("<img>").attr("src", atob(json.files[i].source)).appendTo(".gallery-item" + i);
+            }
+            else if(json.files[i].mimeType == "video") {
+                $(".gallery-content").append("<video autoplay muted loop class='gallery-item gallery-item" + i + "'></video>");
+                $("<source>").attr("src", atob(json.files[i].source)).appendTo(".gallery-item" + i);
+            }
+            $.each(json.files[i].tags, function(t, tag) {
+                $(".gallery-item" + i).addClass(atob(json.files[i].tags[t]));
+            });
         });
     });
 });
